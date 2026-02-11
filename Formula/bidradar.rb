f8e4514f7df5d085f71d4016c1441bfce02b996a
Rubens Praser Junior <praser@gmail.com>
2026-02-11 19:14:56 -0300

    Initialize repo

---
+++ Formula/bidradar.rb
@@ -0,0 +1,19 @@
+class Bidradar < Formula
+  desc "CLI tool for tracking real estate auction offers from CEF"
+  homepage "https://github.com/praser/bidradar"
+  url "https://github.com/praser/bidradar/releases/download/v1.0.0/bidradar-1.0.0.tar.gz"
+  sha256 "PLACEHOLDER"
+  license "ISC"
+
+  depends_on "node@22"
+
+  def install
+    libexec.install "dist/bin/index.js"
+    (bin/"bidradar").write_env_script libexec/"index.js",
+      PATH: "#{Formula["node@22"].opt_bin}:$PATH"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/bidradar --version")
+  end
+end
